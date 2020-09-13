4907c2ee90f3e1284e31f5b28f6db30771b6ede5
MrBusiness87 <bbartula87@gmail.com>
2020-09-12 21:48:37 -0400

    09/12/2020-Added gitignore for node modules, and trying date code in workout.js

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-
+  day: {
+    type: Date,
+    default: Date.now
+  },
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
